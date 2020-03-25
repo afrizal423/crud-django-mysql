a4763099a93293804a48c0a458dca00811466ff7
afrizal423 <afrizal2499@gmail.com>
2020-03-25 13:01:45 +0700

    first
    Upload lengkapnya besok kamis(26/03/20) siang/sore

---
+++ CRUDframework/urls.py
@@ -0,0 +1,29 @@
+"""CRUDframework URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from biodata import views  
+
+urlpatterns = [
+    path('',views.emp),
+    path('tmbhjenjang',views.tmbhjenjang),
+    path('editbio/<int:id>',views.edit),
+    # path('updatebio/<int:id>',views.updatebio),
+    path('editjenjang/<int:id>',views.editjenjang),
+    path('hapusjenjang/<int:id>',views.hapusjenjang),
+    # path('updatejenjang/<int:id>',views.updatejenjang),
+    path('admin/', admin.site.urls),
+]
